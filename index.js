e20db3f3bc49125da24c405c48443211155c6cc2
Michael <mike.l_42@hotmail.com>
2018-02-03 11:28:23 -0500

    Aesthetics
    Added last collected
    Icon and title

--- index.js
+++ index.js
@@ -12,15 +12,32 @@ firebase.initializeApp(config);
 
 var firebaseGarbageRef = firebase.database().ref().child("status").child("garbage");
 var firebaseRecycleRef = firebase.database().ref().child("status").child("recycle");
+var textPadding = 18;
 
 firebaseGarbageRef.on('value', function(snap){
   var garbageText = document.getElementById("garbage-text");
   garbageText.innerText = snap.val() + "%";
+  if (parseInt(garbageText.innerText) < textPadding){
+    document.documentElement.style.setProperty(`--textbumpLessG`, "10px");
+  }else{
+    document.documentElement.style.setProperty(`--textbumpLessG`, "22px");
+  }
   document.documentElement.style.setProperty(`--garbage-width`, ""+garbageText.innerText+"");
 })
 
 firebaseRecycleRef.on('value', function(snap){
   var recycleText = document.getElementById("recycle-text");
+  var time = document.getElementById("timeyboye");
+  var options = {
+    weekday: "long", year: "numeric", month: "short",
+    day: "numeric", hour: "2-digit", minute: "2-digit"
+  };
+  time.innerText = "Last Collected:  "+new Date(2018, 1, 1, 5, 2).toLocaleTimeString("en-us", options);
   recycleText.innerText = snap.val() + "%";
+  if (parseInt(recycleText.innerText) < textPadding){
+    document.documentElement.style.setProperty(`--textbumpLessR`, "10px");
+  }else{
+    document.documentElement.style.setProperty(`--textbumpLessR`, "22px");
+  }
   document.documentElement.style.setProperty(`--recycle-width`, ""+recycleText.innerText+"");
 })
