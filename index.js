07fa8cb239981c9141e5cc381b5a4d06f22eccc7
Michael <mike.l_42@hotmail.com>
2018-02-04 03:26:20 -0500

    Aesthetics
    Slide animations between modal panels added
    Modal Panels Added
    New JQuery slide library used

--- index.js
+++ index.js
@@ -15,33 +15,40 @@ var firebaseRecycleRef = firebase.database().ref().child("status").child("recycl
 var textPadding = 18;
 
 firebaseRecycleRef.on('value', function(snap){
-  var recycleText = document.getElementById("recycle-text");
   var time = document.getElementById("timeyboye");
   var options = {
     weekday: "long", year: "numeric", month: "short",
     day: "numeric", hour: "2-digit", minute: "2-digit"
   };
   time.innerText = "Last Collected:  "+new Date(2018, 1, 1, 5, 2).toLocaleTimeString("en-us", options);
-  recycleText.innerText = snap.val() + "%";
-  if (parseInt(recycleText.innerText) >= 85){
+
+  var recycleText = document.getElementById("recycle-text");
+  var recycleDouble = snap.val();
+  recycleText.innerText = Math.round(parseFloat(recycleDouble));
+  recycleText.innerText = parseInt(recycleText.innerText)-(parseInt(recycleText.innerText)%5);
+  if (recycleText.innerText >= 85){
     document.documentElement.style.setProperty(`--mapIconURL`, "url('images/mapIcon3.png')");
-  }else if (parseInt(recycleText.innerText) > 40 && parseInt(recycleText.innerText) < 85){
+  }else if (recycleText.innerText > 40 && recycleText.innerText < 85){
     document.documentElement.style.setProperty(`--mapIconURL`, "url('images/mapIcon2.png')");
   }else{
     document.documentElement.style.setProperty(`--mapIconURL`, "url('images/mapIcon1.png')");
   }
+  recycleText.innerText = recycleText.innerText +"%";
   document.documentElement.style.setProperty(`--recycle-width`, ""+recycleText.innerText+"");
 })
 
 firebaseGarbageRef.on('value', function(snap){
   var garbageText = document.getElementById("garbage-text");
-  garbageText.innerText = snap.val() + "%";
-  if (parseInt(garbageText.innerText) >= 85){
+  var garbageDouble = snap.val();
+  garbageText.innerText = Math.round(parseFloat(garbageDouble));
+  garbageText.innerText = parseInt(garbageText.innerText)-(parseInt(garbageText.innerText)%5);
+  if (Math.round(parseFloat(garbageText.innerText)) >= 85){
     document.documentElement.style.setProperty(`--mapIconURL`, "url('images/mapIcon3.png')");
-  }else if (parseInt(garbageText.innerText) > 40 && parseInt(garbageText.innerText) < 85){
+  }else if (garbageText.innerText > 40 && garbageText.innerText < 85){
     document.documentElement.style.setProperty(`--mapIconURL`, "url('images/mapIcon2.png')");
   }else{
     document.documentElement.style.setProperty(`--mapIconURL`, "url('images/mapIcon1.png')");
   }
+  garbageText.innerText = garbageText.innerText +"%";
   document.documentElement.style.setProperty(`--garbage-width`, ""+garbageText.innerText+"");
 })
